58a3d09a6615be6308ce1bd8e2a1c069f79cf9fa
Piligrim2121 <danilpanin2002@yandex.ru>
2021-10-13 08:09:18 +0500

    Добавьте файлы проекта.

---
+++ Lo-Fi Shop/Lo-Fi Shop/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lo_Fi_Shop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainMenuPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lo-Fi Shop/Lo-Fi Shop/MainMenuPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lo_Fi_Shop
+{
+    public partial class MainMenuPage : ContentPage
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+            
+           test();
+        }
+        
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+       public void test()
+      {
+            anim.IsAnimationPlaying = true;
+           //BacgroundGif.IsAnimationPlaying = true;
+            // sl.Children.Add(new Image { Source = "Resources/drawable/logo.gif", 
+            //   isAnimationPlaying});
+        }
+    }
+}
