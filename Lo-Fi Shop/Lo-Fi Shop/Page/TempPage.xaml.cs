d3a2582a25ad81f450ec26c3f8cb69a9d3fb7982
Skamky <moskam@tuta.io>
2021-10-16 14:17:27 +0500

    Попытка реализации создания файла

---
+++ Lo-Fi Shop/Lo-Fi Shop/Page/TempPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+// Оригинальный код https://metanit.com/sharp/xamarin/6.1.php
+namespace Lo_Fi_Shop.Page
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class TempPage : ContentPage
+{
+        // путь к месту хранения файлов
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public TempPage()
+    {
+        InitializeComponent();
+    }
+        
+        
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateFileList();
+        }
+        // сохранение текста в файл
+        async void Save(object sender, EventArgs args)
+        {
+            string filename = fileNameEntry.Text;
+            if (String.IsNullOrEmpty(filename)) return;
+            // если файл существует
+            if (File.Exists(Path.Combine(folderPath, filename)))
+            {
+                // запрашиваем разрешение на перезапись
+                bool isRewrited = await DisplayAlert("Подтверждение", "Файл уже существует, перезаписать его?", "Да", "Нет");
+                if (isRewrited == false) return;
+            }
+            // перезаписываем файл
+            File.WriteAllText(Path.Combine(folderPath, filename), textEditor.Text);
+            // обновляем список файлов
+            UpdateFileList();
+        }
+        void FileSelect(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (args.SelectedItem == null) return;
+            // получаем выделенный элемент
+            string filename = (string)args.SelectedItem;
+            // загружем текст в текстовое поле
+            textEditor.Text = File.ReadAllText(Path.Combine(folderPath, (string)args.SelectedItem));
+            // устанавливаем название файла
+            fileNameEntry.Text = filename;
+            // снимаем выделение
+            filesList.SelectedItem = null;
+
+        }
+        void Delete(object sender, EventArgs args)
+        {
+            // получаем имя файла
+            string filename = (string)((MenuItem)sender).BindingContext;
+            // удаляем файл из списка
+            File.Delete(Path.Combine(folderPath, filename));
+            // обновляем список файлов
+            UpdateFileList();
+        }
+        // обновление списка файлов
+        void UpdateFileList()
+        {
+            // получаем все файлы
+            filesList.ItemsSource = Directory.GetFiles(folderPath).Select(f => Path.GetFileName(f));
+            // снимаем выделение
+            filesList.SelectedItem = null;
+        }
+    }
+}
---
+++ Lo-Fi Shop/Lo-Fi Shop/classes/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.IO;
+using System.Linq;
+
+namespace Lo_Fi_Shop.classes
+{
+    class Class1
+    {
+        
+
+    }
+}
